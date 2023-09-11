efd8bbd47409ba8d8782c3d2697e069cb1c70393
gonzalo morales <gonzamorales2004@gmail.com>
2023-09-11 17:14:28 -0300

    final

--- scrip.js
+++ scrip.js
@@ -1,12 +1,21 @@
-let btn=document.getElementById("btn");
-const url="https://jsonplaceholder.typicode.com/users";
+let form=document.getElementById("form");
 let name=document.getElementById("name");
 let lastName=document.getElementById("lastname");
-let date=document.getElementById("date")
+let date=document.getElementById("date");
 
-btn.addEventListener("click", {
-    fetch(url, {
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    fetch("https://jsonplaceholder.typicode.com/users",{
         method:"POST",
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            'Nombre': name.value,
+            'Apellido': lastName.value,
+            'Fecha de nacimiento': date.value,
+        }),
+        headers:{
+            "content-type": "application/json; charset=UTF-8"
+        }
     })
-})
+    .then(response => response.json())
+    .then(json => console.log(json))
+});
